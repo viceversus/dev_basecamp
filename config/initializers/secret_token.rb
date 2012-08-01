ca5cd9e3f16ccfe1b833c4a48edf77fbe3aa3985
dbc-apprentice <apprentice@devbootcamp.com>
2012-08-01 10:36:33 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DevBasecamp::Application.config.secret_token = 'cb28a7969ad3bc6264bcefb2b38381351517f2104c2f9030f1fdedb05f5370bf544dffc2e60b0025b64a0ffcee91adf16353b48ab087d511d3756980cce62dba'
